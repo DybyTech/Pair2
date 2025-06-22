38efe367980b34e96a5e0473ad371a371e5c7d94
DybyTech <dybytech00@gmail.com>
2025-06-22 11:51:34 -0400

    Update pair.js

--- pair.js
+++ pair.js
@@ -10,7 +10,7 @@ const {
     delay,
     makeCacheableSignalKeyStore,
     Browsers
-} = require("@whiskeysockets/baileys");
+} = require("maher-zubair-baileys");
 
 function removeFile(FilePath){
     if(!fs.existsSync(FilePath)) return false;
