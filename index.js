c7dd45c1554e98567a41a363870f80e73197cbb4
DybyTech <dybytech00@gmail.com>
2025-06-22 12:03:07 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,36 @@
 const express = require('express');
-const app = express();
-__path = process.cwd()
 const bodyParser = require("body-parser");
+const path = require('path');
+const app = express();
 const PORT = process.env.PORT || 8000;
-let server = require('./qr'),
-    code = require('./pair');
+
 require('events').EventEmitter.defaultMaxListeners = 500;
-app.use('/qr', server);
-app.use('/code', code);
-app.use('/pair',async (req, res, next) => {
-res.sendFile(__path + '/pair.html')
-})
-app.use('/',async (req, res, next) => {
-res.sendFile(__path + '/main.html')
-})
+
+// Importation des routes
+const qrRoute = require('./qr');
+const pairCodeRoute = require('./pair');
+
+// Middleware pour body-parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// Routes API
+app.use('/qr', qrRoute);
+app.use('/code', pairCodeRoute);
+
+// Routes HTML
+app.get('/pair', (req, res) => {
+  res.sendFile(path.join(__dirname, 'pair.html'));
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'main.html'));
+});
+
+// Lancement du serveur
 app.listen(PORT, () => {
-    console.log(`
-Don't Forgot To Give Star
+  console.log(`\n✅ Server running on http://localhost:${PORT}`);
+  console.log(`⭐ N'oublie pas de mettre une étoile au repo !`);
+});
 
- Server running on http://localhost:` + PORT)
-})
-
-module.exports = app
+module.exports = app;
